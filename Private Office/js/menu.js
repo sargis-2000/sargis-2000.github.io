5ad6985a4a57f8193fb11d86fe7784f97ced233b
sargis-2000 <106433630+sargis-2000@users.noreply.github.com>
2023-07-30 21:12:03 +0400

    Delete Private Office directory

--- Private Office/js/menu.js
+++
@@ -1,6 +0,0 @@
-const menuCloseBtn = document.querySelector('.sidebar__close-btn');
-const menuBtn = document.querySelector('.burger-btn');
-const menu = document.querySelector('.sidebar');
-
-menuBtn.onclick = () => menu.classList.add('active');
-menuCloseBtn.onclick = () => menu.classList.remove('active');
