50a83a6f72f94fa076c60153c3e0b9efb2d0e0e2
sargis-2000 <106433630+sargis-2000@users.noreply.github.com>
2023-08-19 14:24:33 +0400

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -2,14 +2,24 @@ const formItems = document.querySelectorAll('.form-items__item');
 
 formItems.forEach(item => {
     const input = item.querySelector('input');
-    input.onfocus = () => {
+    input.onfocus = function() {
+        const parent = this.parentNode;
+        const path = parent.nextElementSibling.querySelectorAll('path');
+        path.forEach(item => item.classList.add('active'));
         item.style.outline = '1px solid #FF922D';
     }
-    input.onblur = () => {
+    input.onblur = function() {
+        const parent = this.parentNode.nextElementSibling;
+        const path = parent.querySelectorAll('path');
+        path.forEach(item => item.classList.remove('active'));
         if(input.value !== '') {
-            input.nextElementSibling.textContent = input.value;
-            input.value = '';
+            input.classList.add('active');
+            item.removeAttribute('style');
+            input.nextElementSibling.classList.add('active');
+        } else {
+            item.removeAttribute('style');
+            input.classList.remove('active');
+            input.nextElementSibling.classList.remove('active');
         }
-        item.removeAttribute('style');
     }
 });
